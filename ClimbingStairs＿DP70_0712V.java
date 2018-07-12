83c12b46526c2c6451757e6dc86345d4e7013d76
LucyKuo <yujuko@gmail.com>
2018-07-12 14:42:07 +0800

    7/12

---
+++ ClimbingStairs＿DP70_0712V.java
@@ -0,0 +1,69 @@
+
+
+//You are climbing a stair case. It takes n steps to reach to the top.
+//
+//Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+//
+//    Note: Given n will be a positive integer.
+//
+//    Example 1:
+//    Input: 2
+//    Output: 2
+//    Explanation: There are two ways to climb to the top.
+//      1. 1 step + 1 step
+//      2. 2 steps
+//
+//    Example 2:
+//    Input: 3
+//    Output: 3
+//    Explanation: There are three ways to climb to the top.
+//      1. 1 step + 1 step + 1 step
+//      2. 1 step + 2 steps
+//      3. 2 steps + 1 step
+//
+
+
+//Solutions:
+class Solution {
+    public int climbStairs(int n) {
+
+        if (n<0) {return -1 ;}
+        if (n==0) {return 0 ;}
+        if (n==1) {return 1 ;}
+        if (n==2) {return 2 ;}
+
+
+        else{
+            int first = 1;
+            int second = 2;
+            int total = 0;
+            for (int i = 0; i< n-2 ; i++){
+                total = first + second;
+                first = second;
+                second = total;
+            }return total;
+        }
+    }
+}
+
+//Fibonacci: 以1,2起始的Fibonacci數列（遞迴）
+// -->TINE LIMIT EXCEEDED.
+
+class Solution {
+    public int climbStairs(int n) {
+        if (n < 0) {
+            return -1;
+        }
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        } else {
+            return climbStairs(n - 1) + climbStairs(n - 2);
+        }
+    }
+}
