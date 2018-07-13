f633f48b63393dbf76fc2e0e1537109827dc7bbe
LucyKuo <yujuko@gmail.com>
2018-07-13 17:00:39 +0800

    0713

---
+++ MaximumSubarray_DP53_0713V.java
@@ -0,0 +1,32 @@
+//53. Maximum Subarray
+//        DescriptionHintsSubmissionsDiscussSolution
+//        Given an integer array nums, find the contiguous subarray (containing at least one number)
+//        which has the largest sum and return its sum.
+//
+//        Example:
+//
+//        Input: [-2,1,-3,4,-1,2,1,-5,4],
+//        Output: 6
+//        Explanation: [4,-1,2,1] has the largest sum = 6.
+//        Follow up:
+//
+//        If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+
+
+class Solution {
+
+    public int maxSubArray(int[] nums) {
+
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        int max = dp[0];
+
+        for (int i=1 ; i< nums.length ; i++){
+            dp[i] = (dp[i-1] > 0? dp[i-1]:0) + nums[i];
+            max = Math.max(dp[i],max);
+        }return max;
+    }
+
+
+}
+//DP Explanation: http://localhost:4000/2018/07/12/DynamicProgramming/#more
