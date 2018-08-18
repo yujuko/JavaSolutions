8571378ad288aab8946150d677f8c69944e4fb15
LucyKuo <yujuko@gmail.com>
2018-08-18 16:54:46 -0400

    8/18

--- AddBinary_S67_0707V.java
+++ AddBinary_S67_0707V.java
@@ -15,20 +15,20 @@
 
 class Solution {
     public String addBinary(String a, String b) {
-        StringBuilder adds = new StringBuilder();
-        int i = a.length()-1 , j = b.length()-1 , carry = 0;
+        StringBuilder adds = new StringBuilder(); //Stringbuilder 可以append 及 reverse
+        int i = a.length()-1 , j = b.length()-1 , carry = 0; //從最高為加到最低位（右到左）
 
-        while ( i>=0 || j>=0 ){
-            int sum = carry;
+        while ( i>=0 || j>=0 ){ //左右顛倒
+            int sum = carry; //上一輪要進位的放入下一輪再加
             if ( i>=0 ) sum += a.charAt(i--) - '0';
-            if ( j>=0 ) sum += a.charAt(j--) - '0';
+            if ( j>=0 ) sum += a.charAt(j--) - '0'; //把字元的ASCII剪掉0的ASCII會是原本的數字
 
-            adds.append(sum % 2);
-            carry = sum / 2;
+            adds.append(sum % 2); //不用進位/進位後剩的部分先接上
+            carry = sum / 2; //要進位的部分
 
         }
 
-        if( carry!=0 ) adds.append( carry );
-        return adds.reverse().toString();
+        if( carry!=0 ) adds.append( carry ); //街上最後的進位
+        return adds.reverse().toString(); //反轉
     }
 }
