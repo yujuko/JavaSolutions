bf15ea3378a3f32dc2dd60f17f07bccd7aa61980
LucyKuo <yujuko@gmail.com>
2018-08-18 18:06:25 -0400

    818

--- ArrayPairSum_A561_0706.java
+++ ArrayPairSum_A561_0706.java
@@ -1,4 +1,8 @@
-//Given an array of 2n integers, your task is to group these integers into n pairs of integer, say (a1, b1), (a2, b2), ..., (an, bn) which makes sum of min(ai, bi) for all i from 1 to n as large as possible.
+//561. Array Partition I
+
+//Given an array of 2n integers, your task is to group these integers into n pairs of
+//integer, say (a1, b1), (a2, b2), ..., (an, bn) which makes sum of min(ai, bi) for
+//all i from 1 to n as large as possible.
 //
 //Example 1:
 //Input: [1,4,3,2]
@@ -16,12 +20,9 @@ class Solution {
     public int arrayPairSum(int[] nums) {
         Arrays.sort(nums);
         int sum = 0;
-        int  j;
         
-        for ( j=0 ; j < nums.length-1 ; j++){
-            if ( j % 2 == 0 ){
+        for (int j=0 ; j < nums.length-1 ; j+=2){
                 sum += nums[j];
-            }
         }
         return sum;
     }
