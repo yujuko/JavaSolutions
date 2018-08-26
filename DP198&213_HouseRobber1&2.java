dd286928d5439e6aaf04ad2085f3ca0c89b5f756
LucyKuo <yujuko@gmail.com>
2018-08-26 10:47:52 -0400

    -

--- DP198&213_HouseRobber1&2.java
+++ DP198&213_HouseRobber1&2.java
@@ -23,7 +23,37 @@
 //        Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
 //        Total amount you can rob = 2 + 9 + 1 = 12.
 
+My Solution:
+public class Main {
+    
+    public static void main(String[] args) {
+        int[] nums = {7,5,4,2,8,9};
+        
+        int currob = 0; //現在搶與不搶的結論
+        int notrob = 0;
+        int rob = 0;
+        
+        for( int i=0 ; i<nums.length ; i++){
+            rob = notrob + nums[i]; //若要搶[i]間,[i-1]必不可搶(notrob為[i-1]不搶的最大值)則搶這間結果應為[i-1]不搶的最大值再加上這一間
+            notrob = currob; //不搶[i]間的話,由上一輪最大值延續比較
+            currob = Math.max(rob, notrob); //比較後為這輪結果
+            System.out.println(rob+" "+notrob+" "+currob);
+            
+        }
+        System.out.println( Math.max(rob,notrob)); //return currob;
+    }
+}
+//執行結果:
+//7   0   7
+//5   7   7
+//11  7   11
+//9   11  11
+//19  11  19
+//20  19  20
 
+//20
+
+Solution 2:
 public class Main {
 
     public static void main(String[] args) {
