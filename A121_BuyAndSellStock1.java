55d7f76c1d26534246da1cb849980c96497cb00c
LucyKuo <yujuko@gmail.com>
2019-01-02 17:52:12 -0500

    update122

---
+++ A121_BuyAndSellStock1.java
@@ -0,0 +1,51 @@
+//Say you have an array for which the ith element is the price of a given stock on day i.
+//
+//        If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+//
+//        Note that you cannot sell a stock before you buy one.
+//
+//        Example 1:
+//
+//        Input: [7,1,5,3,6,4]
+//        Output: 5
+//        Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+//        Not 7-1 = 6, as selling price needs to be larger than buying price.
+//        Example 2:
+//
+//        Input: [7,6,4,3,1]
+//        Output: 0
+//        Explanation: In this case, no transaction is done, i.e. max profit = 0.
+
+//My solution: 430 ms
+class Solution {
+    public int maxProfit(int[] prices) {
+
+        int profit = 0;
+        for(int i=0 ; i< prices.length-1 ; i++){
+            for(int j=i+1 ; j<prices.length ; j++){
+                if(prices[j]- prices[i]>profit)
+                    profit = prices[j]- prices[i];
+            }
+        }
+        return profit;
+    }
+}
+
+//The logic to solve this problem is same as "max subarray problem" using Kadane's Algorithm.
+//Since no body has mentioned this so far, I thought it's a good thing for everybody to know.
+//All the straight forward solution should work, but if the interviewer twists the question
+//slightly by giving the difference array of prices, Ex: for {1, 7, 4, 11}, if he gives {0, 6, -3, 7},
+//you might end up being confused.
+//Here, the logic is to calculate the difference (maxCur += prices[i] - prices[i-1]) of the
+// original array, and find a contiguous subarray giving maximum profit. If the difference falls below 0, reset it to zero.
+
+    public int maxProfit(int[] prices) {
+        int maxCur = 0, maxSoFar = 0;
+        for(int i = 1; i < prices.length; i++) {
+            maxCur = Math.max(0, maxCur += prices[i] - prices[i-1]);
+            maxSoFar = Math.max(maxCur, maxSoFar);
+        }
+        return maxSoFar;
+    }
+//*maxCur = current maximum value
+//*maxSoFar = maximum value found so far
