3620c5d2b1a9d4688499cf48eed7cef203a51a82
LucyKuo <yujuko@gmail.com>
2019-01-11 15:53:33 -0500

    S929

---
+++ S929_UniqueEmailAddresses.java
@@ -0,0 +1,93 @@
+//929. Unique Email Addresses
+//
+//        Every email consists of a local name and a domain name, separated by the @ sign.
+//
+//        For example, in alice@leetcode.com, alice is the local name, and leetcode.com is the domain name.
+//
+//        Besides lowercase letters, these emails may contain '.'s or '+'s.
+//
+//        If you add periods ('.') between some characters in the local name part of an email address, mail sent there will be forwarded to the same address without dots in the local name.  For example, "alice.z@leetcode.com" and "alicez@leetcode.com" forward to the same email address.  (Note that this rule does not apply for domain names.)
+//
+//        If you add a plus ('+') in the local name, everything after the first plus sign will be ignored. This allows certain emails to be filtered, for example m.y+name@email.com will be forwarded to my@email.com.  (Again, this rule does not apply for domain names.)
+//
+//It is possible to use both of these rules at the same time.
+//
+//        Given a list of emails, we send one email to each address in the list.  How many different addresses actually receive mails?
+//
+//
+//
+//        Example 1:
+//
+//        Input: ["test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"]
+//        Output: 2
+//        Explanation: "testemail@leetcode.com" and "testemail@lee.tcode.com" actually receive mails
+//
+//
+//        Note:
+//
+//        1 <= emails[i].length <= 100
+//        1 <= emails.length <= 100
+//        Each emails[i] contains exactly one '@' character.
+
+//Runtime: 90 ms
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int numUniqueEmails(String[] emails) {
+        Set<String> emailsSet = new HashSet<>();
+
+        for(String S: emails){
+            int index = S.indexOf('@');
+            String temp1 = S.substring(0, index);
+            String temp2 = S.substring(index);
+
+            // if(temp1.contains(".")) --> Why wrong?
+            temp1 = temp1.replaceAll(".","");
+            if(temp1.contains("+"))
+                temp1 = temp1.substring(0, temp1.indexOf("+"));
+            S = temp1+temp2;
+            emailsSet.add(S);
+        }
+        return emailsSet.size();
+    }
+}
+//Split solution
+public int numUniqueEmails(String[] emails) {
+    if(emails.length==0) return 0;
+    Set<String> hs = new HashSet<>();
+    for(String email: emails)
+    {
+        String[] arr = email.split("@");
+        String local = arr[0].substring(0, arr[0].indexOf('+')).replace(".", "");
+        hs.add(local+"@"+arr[1]);
+    }
+    return hs.size();
+}
+
+// Runtime error:
+// class Solution {
+//     public int numUniqueEmails(String[] emails) {
+//         Set<String> emailsSet = new HashSet<>();
+
+//         for(String S: emails){
+//             int index = S.indexOf('@');
+//             for (int i = 0 ; i< index ; i++){
+//                 if (S.charAt(i) == '.'){ -->WHY WRONG? StringIndexOutOfRange
+//                     String temp1 = S.substring(0,i);
+//                     String temp2 = S.substring(i+1);
+//                     S = temp1.concat(temp2);
+//                     // S.replace(S.charAt(i),'');
+//                 }
+//                 if (S.charAt(i) == '+'){
+//                     String temp1 = S.substring(0,i);
+//                     String temp2 = S.substring(index);
+//                     S = temp1.concat(temp2);
+//                 }
+//               emailsSet.add(S);
+//             }
+//         }
+
+//     return emailsSet.size();
+//     }
+// }
