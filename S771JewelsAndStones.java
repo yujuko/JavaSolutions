3953af177a529f44fb63a3d9e4d200093affe36e
LucyKuo <yujuko@gmail.com>
2019-01-11 14:50:21 -0500

    update S771

--- S771JewelsAndStones.java
+++ S771JewelsAndStones.java
@@ -32,7 +32,19 @@ class Solution {
     }
 }
 
-//discussion:
+//one_line solution:
 public int numJewelsInStones(String J, String S) {
    return S.replaceAll("[^" + J + "]", "").length();
+}
+
+//HashSet solution:
+public int numJewelsInStones(String J, String S) {
+    int res = 0;
+    Set setJ = new HashSet();
+    for (char j: J.toCharArray())
+        setJ.add(j);
+    for (char s: S.toCharArray())
+        if (setJ.contains(s))
+        res++;
+    return res;
 }
